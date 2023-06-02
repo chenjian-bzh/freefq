58d91f218f3b3cb71bbc6b0f50fd455b507b3a8f
Lsong <song940@gmail.com>
2023-06-02 14:24:06 +0800

    update

--- run.js
+++ run.js
@@ -8,17 +8,111 @@ const { writeFile } = fs.promises;
 const writeYaml = (filename, obj) =>
   writeFile(filename, YAML.dump(obj));
 
-async function convert_sub(url, type = "clash") {
-  const response = await fetch(`https://api.dler.io/sub?target=${type}&url=${encodeURIComponent(url)}`)
-  const data = await response.text();
-  return YAML.load(data);
+function parseURL(url) {
+  const pattern = /^((\w+):\/\/)?((.+)?@)?([^\/\?:]+):?(\d+)?(\/?[^\?#]+)?\??([^#]+)?#?(.*)/;
+  const match = pattern.exec(decodeURIComponent(url));
+  return Object.entries({
+    url: 0,
+    protocol: 2,
+    auth: 4,
+    host: 5,
+    port: 6,
+    path: 7,
+    querystring: 8,
+    hash: 9,
+  }).reduce((out, [k, v]) => (out[k] = match[v] || '', out), {});
+}
+
+const handlers = {}
+
+const registerHandler = (proto, handler) => {
+  handlers[proto] = handler
+};
+
+registerHandler('vmess', data => {
+  const vmess = JSON.parse(Buffer.from(data, 'base64'));
+  const output = {
+    type: "vmess",
+    name: vmess.ps,
+    server: vmess.add,
+    port: vmess.port,
+    uuid: vmess.id,
+    alterId: vmess.aid,
+    cipher: "auto",
+    network: vmess.net,
+    tls: vmess.tls == 'tls',
+    'skip-cert-verify': true,
+  };
+  if (vmess.sni) {
+    output.sni = vmess.sni;
+  }
+  if (vmess.alpn) {
+    output.alpn = vmess.alpn;
+  }
+  const opts = `${vmess.net}-opts`;
+  output[opts] = output[opts] || {};
+  if (vmess.host) {
+    output[opts]["host"] = vmess.host;
+    output[opts]["headers"] = {
+      "host": vmess.host,
+    }
+  }
+  if (vmess.path) {
+    output[opts]["path"] = vmess.path;
+  }
+  return output;
+});
+
+registerHandler('trojan', (_, link) => {
+  const data = parseURL(link);
+  return {
+    name: data.hash,
+    type: data.protocol,
+    server: data.host,
+    port: data.port,
+    password: data.auth,
+    'skip-cert-verify': true,
+    alpn: ["h2", "http/1.1"]
+  };
+});
+
+const ss = (_, link) => {
+  const data = parseURL(link);
+  const [cipher, password] = Buffer.from(data.auth, 'base64').toString().split(':');
+  return {
+    name: data.hash,
+    type: data.protocol,
+    server: data.host,
+    port: data.port,
+    cipher,
+    password,
+    udp: true,
+  };
+};
+
+registerHandler('ss', ss);
+registerHandler('ssr', ss);
+
+function parse_link(link) {
+  const [proto, content] = link.split('://');
+  if (proto in handlers) {
+    return handlers[proto](content, link);
+  } else {
+    console.log(`Unknown protocol: ${proto}`);
+  }
+}
+
+async function convert_sub(content) {
+  const links = content.split(/\r?\n/g).filter(Boolean);
+  return Promise.all(links.map(parse_link));
 }
 
 async function main() {
   for (const [name, url] of Object.entries(links)) {
-    const data = await convert_sub(url);
-    writeYaml(`./proxies/${name}.yaml`, data.proxies);
-    console.log(name);
+    const response = await fetch(url)
+    const content = Buffer.from(await response.text(), 'base64').toString();
+    const proxies = await convert_sub(content);
+    writeYaml(`./proxies/${name}.yaml`, { proxies });
   }
 }
 
